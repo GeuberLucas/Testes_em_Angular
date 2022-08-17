8385f9f1d71ff234ef0c81c3fd9c0db9ac50948f
Geuber <geuber@supersimplescred.com>
2022-08-17 18:32:22 -0300

    testes

--- lugar de assinatura/assinatura/src/app/pages/assignature/assignature.component.ts
+++ lugar de assinatura/assinatura/src/app/pages/assignature/assignature.component.ts
@@ -6,22 +6,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignature.component.scss']
 })
 export class AssignatureComponent implements OnInit {
+  context
 
 
   constructor() {
     document.addEventListener('DOMContentLoaded',()=>{
-      const tela =document.querySelector('canvas');
-      const context = tela.getContext("2d")
+      const tela=document.querySelector('canvas');
+      this.context = tela.getContext("2d")
       tela.width = 700
       tela.height= 500;
 
-      context.beginPath();
-      const
-      context.moveTo(10,10);
-      context.lineTo(350,250);
-      context.stroke()
+      const pincel={
+        ativo:false,
+        movendo:false,
+        pos:{x:0,y:0},
+        posAnterior:null
+      }
+
+      const draw =(linha) =>{
+          this.context.beginPath();
+        this.context.moveTo(linha.posAnterior.x,linha.posAnterior.y);
+        this.context.lineTo(linha.pos.x,linha.pos.y);
+        this.context.stroke()
+      }
+
+      tela.onmousedown = (evento) =>{
+        pincel.ativo=true
+
+      }
+
+      tela.onmouseup = (evento) =>{
+        pincel.ativo=false
+
+      }
+
+      tela.onmousemove = (evento) =>{
+        pincel.pos.x= evento.clientX
+        pincel.pos.y= evento.clientY
+        pincel.movendo= true
+
+      }
+      const ciclo = ()=>{
+        pincel.ativo && pincel.movendo && pincel.posAnterior ){
+          draw({pos:pincel.pos,posAnterior:pincel.posAnterior})
+          pincel.movendo =false
+        }
+
+        pincel.posAnterior= {x: pincel.pos.x,y:pincel.pos.y}
+        setTimeout(ciclo, 10);
+      }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+      ciclo()
+
     })
   }
+
 
   ngOnInit(): void {
 
